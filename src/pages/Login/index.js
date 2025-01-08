aea05257eaa053862840959607c214d5512a47cd
jose.bento <jose.natal@ibrowse-sds.com.br>
2025-01-08 00:07:20 -0300

    Integrando a Tela de Login a API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom'
 import './styles.css';
+
+import api from '../../services/api'
 
 import logoImage from '../../assets/logo.svg'
 import padlock from '../../assets/padlock.png'
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        const data = {
+            username,
+            password,
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.accessToken)
+
+            navigate('/books')
+        } catch (error) {
+            alert('Login failed! Try agains!')
+        }
+    };
+
     return (
        <div className='login-container'>
         <section className="form">
             <img src={logoImage} alt="Logo"/>
-           <form action="">
+           <form onSubmit={handleLogin}>
                 <h1>Acess your Account</h1>
-                <input placeholder="Username"/>
-                <input type="password" placeholder="Password" />
+                <input 
+                    placeholder="Username"
+                    value={username}
+                    onChange={ e => setUsername(e.target.value)}
+                />
+                <input type="password" placeholder="Password" 
+                    value={password}
+                    onChange={ e => setPassword(e.target.value)}
+                />
 
                 <button className='button' type="submit">Login</button>
            </form>
