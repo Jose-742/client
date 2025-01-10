6ca141f8adec758eac5a8436c5c3f8906f4121b4
jose.bento <jose.natal@ibrowse-sds.com.br>
2025-01-10 00:25:52 -0300

    Integrando a Tela de Listagem de Livros, Delete e o Logout

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,82 +1,100 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, {useState, useEffect, useCallback} from 'react';
+import { useNavigate, Link } from 'react-router-dom'
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
 
+import api from '../../services/api'
+
 import logoImage from '../../assets/logo.svg'
 
 export default function Books(){
+    
+    const [books, setBooks] = useState([]);
+
+    const username = localStorage.getItem('username');
+    const accessToken = localStorage.getItem('accessToken');
+
+    const navigate = useNavigate();
+
+    async function logout(){
+        localStorage.clear();
+        navigate('/')
+    }
+
+     // Função para carregar os livros (memoizada)
+    const loadBooks = useCallback(async () => {
+        try {
+            const response = await api.get('api/book/v1', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+                params: {
+                    page: 1,
+                    size: 4,
+                    direction: 'asc'
+                }
+            });
+            setBooks(response.data._embedded.bookVOList);
+        } catch (err) {
+            console.error('Error fetching books:', err);
+        }
+    }, [accessToken]);
+    
+    // Função para deletar um livro
+    async function deleteBook(id){
+        try{
+            await api.delete(`api/book/v1/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+
+            //setBooks(books.filter(book => book.id !== id))
+            await loadBooks(); 
+        } catch (err) {
+            alert('Delete failed! Try again.')
+        }
+    }
+
+    // Carrega os livros ao montar o componente
+    useEffect(() => {
+        loadBooks();
+    }, [loadBooks]);
+    
     return (
-        
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="Logo" />
-                <span>Welcome, <strong>Jose Natal</strong>!</span>
+                <span>Welcome, <strong>{username.toUpperCase()}</strong>!</span>
                 <Link className='button' to="/book/new">Add New Book</Link>
-                <button type='button'>
+                <button onClick={logout} type='button'>
                     <FiPower size={18} color='#251FC5'/>
                 </button>
             </header>
 
             <h1>Registered Books</h1>
             <ul>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2024</p>
-                    
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5'/>
-                    </button>
+                {books.map(book => (
+                    <li key={book.id}>
+                        <strong>Title:</strong>
+                        <p>{book.title}</p>
+                        <strong>Author:</strong>
+                        <p>{book.author}</p>
+                        <strong>Price:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
+                        <strong>Release Date:</strong>
+                        <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
+                        
+                        <button type='button'>
+                            <FiEdit size={20} color='#251FC5'/>
+                        </button>
 
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5'/>
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2024</p>
-                    
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5'/>
-                    </button>
-
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5'/>
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2024</p>
-                    
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5'/>
-                    </button>
-
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5'/>
-                    </button>
-                </li>
+                        <button onClick={() => deleteBook(book.id)} type='button'>
+                            <FiTrash2 size={20} color='#251FC5'/>
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
 
